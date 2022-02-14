44154755c7faee0fd76d87def86e89f9789421c0
TheMDRobinson <matthewdrobinson@gmail.com>
2022-02-14 13:35:55 +0100

    first commit

---
+++ js/services/shows.js
@@ -0,0 +1,7 @@
+app.factory('shows', ['$http', function($http){
+    return $http.get('https://content.codecademy.com/courses/ltp4/shows-api/shows.json').success(function(data){
+      return data;
+    }).error(function(data){
+      return err;
+    })
+  }])
